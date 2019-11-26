2da51229b9d0062f7e620b41d32d8b7b4070b96d
InspireX96 <xumw1996@gmail.com>
2019-11-25 16:47:04 -0800

    format code

--- python_matlab_socket/python_client.py
+++ python_matlab_socket/python_client.py
@@ -9,6 +9,7 @@ import numpy as np
 
 url_index = 'http://127.0.0.1:5000/'
 
+
 def test_connection():
     """
     test connection
@@ -16,6 +17,7 @@ def test_connection():
     print('*** test connection ***')
     r = requests.get(url_index + 'hello', timeout=2)
     print(r.text)
+
 
 def test_post_json():
     """
@@ -28,13 +30,14 @@ def test_post_json():
     print(r.url)
     print(r.text)
 
+
 def test_post_numpy_array():
     """
     test POST numpy array
     """
     print('*** test POST numpy array ***')
     array = np.random.rand(4, 4)
-    payload = json.dumps(array.tolist())    # convert numpy to json
+    payload = json.dumps(array.tolist())  # convert numpy to json
 
     r = requests.post(url_index + 'post-json', json=payload)
     print(r.url)
@@ -44,6 +47,7 @@ def test_post_numpy_array():
     data = json.loads(r.text)['data']
     print('Retrived data:\n', np.array(json.loads(data)))
 
+
 def test_post_numpy_array_stress_test():
     """
     Stress test of sending numpy arrays
@@ -52,12 +56,12 @@ def test_post_numpy_array_stress_test():
     # test large array
     array = np.random.rand(10000, 10)
     print('Sending large array with size: ', array.shape)
-    payload = json.dumps(array.tolist())    # convert numpy to json
+    payload = json.dumps(array.tolist())  # convert numpy to json
 
     time_start = time.time()
     r = requests.post(url_index + 'post-json', json=payload)
     print('Elasped time: ', time.time() - time_start)
-    
+
 
 if __name__ == '__main__':
     test_connection()
--- python_matlab_socket/python_server.py
+++ python_matlab_socket/python_server.py
@@ -5,18 +5,18 @@ A python server example using Flask
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+
 # test connection
 @app.route('/hello')
 def hello():
     return 'Hello, World'
 
+
 # test get a POST request containing JSON
 @app.route('/post-json', methods=['POST'])
 def post_json_test():
-	input_json = request.get_json()
-	print('Get data: {}, datatype: {}'.format(
-		input_json, type(input_json)))
+    input_json = request.get_json()
+    print('Get data: {}, datatype: {}'.format(input_json, type(input_json)))
 
-	# NOTE: cannot use tuple as key
-	return jsonify({'message': 'request success',
-				    'data': input_json})
+    # NOTE: cannot use tuple as key
+    return jsonify({'message': 'request success', 'data': input_json})
